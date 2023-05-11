a151c726467607f04b0d7cea8882f3259cdbeb43
PreetRai <prithviraimangya@gmail.com>
2023-05-11 06:24:11 -0400

    dockerized

--- my-restful-service/app.js
+++ my-restful-service/app.js
@@ -1,58 +1,46 @@
 const express = require('express');
 const app = express();
+const data = require('./data.json');
 const path = require('path');
-const fs = require('fs');
-app.use(express.static(path.join(__dirname, 'public')));
-// Route to display the collection of video games
-app.get('/games', (req, res) => {
-  fs.readFile(path.join(__dirname, 'games.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      const games = JSON.parse(data);
-      res.json(games);
-    }
-  });
+
+app.get('/users', (req, res) => {
+  res.json({ users: data.users });
 });
-// Route to get a game by ID
+
+app.get('/games', (req, res) => {
+  res.json({ games: data.games });
+});
+
 app.get('/games/:id', (req, res) => {
   const gameId = parseInt(req.params.id);
-  fs.readFile(path.join(__dirname, 'games.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      const games = JSON.parse(data);
-      const game = games.games.find(game => game.id === gameId);
-      if (game) {
-        res.json(game);
-      } else {
-        res.status(404).json({ error: 'Game not found' });
-      }
-    }
-  });
+  const game = data.games.find(game => game.id === gameId);
+
+  if (!game) {
+    return res.status(404).json({ error: 'Game not found' });
+  }
+
+  res.json({ game });
 });
-app.get('/games/genre/:genre', (req, res) => {
-  const genre = req.params.genre.toLowerCase();
-  fs.readFile(path.join(__dirname, 'games.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      const games = JSON.parse(data);
-      const filteredGames = games.filter(game => game.genre.toLowerCase() === genre);
-      res.json(filteredGames);
-    }
-  });
+
+app.get('/users/:userId/orders', (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const user = data.users.find(user => user.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  const userOrders = data.orders.filter(order => order.userId === userId);
+  res.json({ orders: userOrders });
 });
+
 // Serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ readme.txt
@@ -0,0 +1,88 @@
+```
+# Video Game Store
+
+Welcome to the Video Game Store, a web service for managing and retrieving information about video games, users, and orders.
+
+## Description
+
+This web service provides a RESTful API with the following endpoints:
+
+- `GET /games`: Returns a collection of all game records.
+- `GET /users`: Returns a collection of all users in the system.
+- `GET /games/{id}`: Returns a single game record corresponding to the provided ID.
+- `GET /users/{userId}/orders`: Returns a collection of order records for a given user.
+
+## Prerequisites
+
+- Node.js (version 14 or higher)
+- Docker (optional, for containerization)
+
+## Installation
+
+1. Clone the repository:
+
+```
+git clone <repository-url>
+```
+
+2. Navigate to the project directory:
+
+```
+cd video-game-store
+```
+
+3. Install the dependencies:
+
+```
+npm install
+```
+
+## Usage
+
+1. Start the application:
+
+```
+npm start
+```
+
+2. Access the web service:
+
+```
+http://localhost:3000
+```
+
+3. Use the API endpoints to retrieve data about games and orders.
+
+## Docker Support
+
+You can also run the application using Docker. Follow these steps:
+
+1. Build the Docker image:
+
+```
+docker build -t video-game-store .
+```
+
+2. Run the Docker container:
+
+```
+docker run -p 3000:3000 --name video-game-store-app video-game-store
+```
+
+3. Access the web service:
+
+```
+http://localhost:3000
+```
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+```
+
+Feel free to modify the content and structure of the README file according to your specific project requirements.
